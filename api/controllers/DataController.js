8c9e10b8d553cd6f27ec2d518e6e2b6542b48601
jrfoehn <jrfoehn@gmail.com>
2017-06-09 16:50:15 +0200

    controller + model

---
+++ api/controllers/DataController.js
@@ -0,0 +1,36 @@
+/**
+ * DataController
+ *
+ * @description :: Server-side logic for managing Data
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+
+
+var temperature = [];
+var humidity = [];
+var waterLever = [];
+module.exports = {
+  create: function(req, res) {
+    var params = req.params.all();
+    var DateToday, today;
+    today = new Date();
+    Data.create({temperature:params.temperature, waterlevel:params.waterlevel, humidity:params.humidity, date:today}).exec(function (err, finn){
+      if (err) { return res.serverError(err); }
+
+      sails.log('Finn\'s id is:', finn.id);
+      return res.ok();
+    });
+  },
+  getMeasures: function(req, res) {
+    Data.native(function(err, collection) {
+      if (err) return res.serverError(err);
+      collection.find({},{}).toArray(function (err, results) {
+        if (err) return res.serverError(err);
+        return res.json(results);
+      });
+    });
+  },
+
+};
+
---
+++ api/models/Data.js
@@ -0,0 +1,32 @@
+/**
+ * Data.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    temperature: {
+      type: 'INTEGER',
+      required: true,
+      columnName: 'temperature(°C)'
+    },
+    waterlevel: {
+      type: 'INTEGER',
+      required: true,
+      columnName: 'waterlevel(cm)'
+    },
+    humidity: {
+      type: 'INTEGER',
+      required: true,
+      columnName: 'humidity(%)'
+    },
+    date: {
+      type: 'DATETIME',
+      required: true,
+      columnName: 'date'
+    }
+  }
+};
+
